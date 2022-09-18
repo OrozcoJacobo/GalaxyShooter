f0a8009ef23fb97c8c8b12232b8f549586515826
JacoboOrozco <orozcojacobo1@gmail.com>
2022-09-18 15:47:42 -0500

    MinorBugs
    Shooting bug fixed

--- Assets/Scripts/Player/Fire.cs
+++ Assets/Scripts/Player/Fire.cs
@@ -10,6 +10,8 @@ public class Fire : MonoBehaviour
     private bool _isTripleShotActive = false;
     [SerializeField] private AudioClip _laserSoundClip;
     private AudioSource _audioSource;
+    private float _fireRate = 0.5f;
+    private float _canFire = -1f;
 
     private void Start()
     {
@@ -30,15 +32,19 @@ public class Fire : MonoBehaviour
     public void FireLaser()
     {
         _audioSource.clip = _laserSoundClip;
-        if (_isTripleShotActive)
+        if (_isTripleShotActive && Time.time > _canFire)
         {
+            _canFire = Time.time + _fireRate;
             Instantiate(_tripleShotPrefab, transform.position + new Vector3 (0, 4f, 0), transform.rotation);
+            _audioSource.Play();
         }
-        else
+        else if(Time.time > _canFire)
         {
+            _canFire = Time.time + _fireRate;
             Instantiate(_laserPrefab, transform.position + new Vector3(0, 1f, 0), transform.rotation);
+            //_audioSource.Play();
         }
-        _audioSource.Play();
+        
     }
 
     public void TripleShotActive()
